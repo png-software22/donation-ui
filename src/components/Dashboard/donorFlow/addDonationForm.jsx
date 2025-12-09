f7446a5fefca510e17c993cb841bfc71c8442b4f
Gursimran Singh <gursimran.singh@trantorinc.com>
2025-12-09 20:45:12 +0530

    Add: changes

--- src/components/Dashboard/donorFlow/addDonationForm.jsx
+++ src/components/Dashboard/donorFlow/addDonationForm.jsx
@@ -1,4 +1,12 @@
 const AddDonationForm = ({donorDetails}) => {
-    return 'here goes add donation form for ' + donorDetails?.firstName
+    return <><h1>Donation Details</h1> 
+    <h3>You are creating a donation for the user <b>{donorDetails?.firstName}</b></h3>
+    // create a form with the following fields
+    1) Donation method : Online, Cheque, Cash, RTGS
+    2) Reference number (only for the case other than cash)
+    3) Amount
+    4) Date field (if back date is allowed)
+    5) Bacnk name (select: SBI, HDFC, ICICI, AXIS, PNB, BOI, BOB, Other)
+    </>
 }
 export default AddDonationForm
